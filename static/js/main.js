e12b989670472a16b25909f0b663e69c3310a6a9
Ben Beecher <benbeecher@gmail.com>
2014-08-10 23:04:24 -0400

    trying flooring

--- static/js/main.js
+++ static/js/main.js
@@ -3,20 +3,19 @@
 $(document).ready(function(){
    $($('section').get().reverse()).each(function(){
        var $bgobj = $(this); // assigning the object
-       var origin = $bgobj.offset().top;
+       var origin = Math.floor($bgobj.offset().top);
        $bgobj.css('position', 'absolute');
        $bgobj.css({ top:origin });
 
        $(window).scroll(function() {
            //  var yPos = -($window.scrollTop() / $bgobj.data('speed'));
-           var yPos = ($(window).scrollTop());
-           var browserHeight = $(window).height();
-           var cardHeight = $bgobj.height();
+           var yPos = Math.floor(($(window).scrollTop()));
+           var browserHeight = Math.floor($(window).height());
+           var cardHeight = Math.floor($bgobj.height());
 
 
            var stopCheck = origin + (cardHeight - browserHeight);
            var fixHeight = yPos - (cardHeight - browserHeight);
-
 
            if (yPos == origin){
                $bgobj.css({ top:origin });
